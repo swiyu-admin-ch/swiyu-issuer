2c9ebbfe354bd6d5518778fc7d460d97d04766d0
Bruno Zimmermann <bruno.zimmermann@bit.admin.ch>
2024-06-18 13:35:56 +0200

    EID-1626 - Make offer data nullable so it can be deleted after the offer has been redeemed

--- src/main/resources/db/migration/V1_0_0__create-schema.sql
+++ src/main/resources/db/migration/V1_0_0__create-schema.sql
@@ -1,13 +1,13 @@
 CREATE TABLE credential_offer (
-    id uuid NOT NULL,
-    credential_status text NOT NULL,
-    metadata_credential_supported_id text NOT NULL,
-    offer_data json NOT NULL,
-    holder_binding_nonce uuid NOT NULL,
-    access_token uuid, -- Generated when token is fetched
-    offer_expiration_timestamp integer, -- if null, offer is infinite
-    credential_valid_from text, -- if null, credential is valid from the time it's issued
-    credential_valid_until text -- if null, credential does not expire on its own
+                                  id uuid NOT NULL,
+                                  credential_status text NOT NULL,
+                                  metadata_credential_supported_id text NOT NULL,
+                                  offer_data json,
+                                  holder_binding_nonce uuid NOT NULL,
+                                  access_token uuid, -- Generated when token is fetched
+                                  offer_expiration_timestamp integer, -- if null, offer is infinite
+                                  credential_valid_from text, -- if null, credential is valid from the time it's issued
+                                  credential_valid_until text -- if null, credential does not expire on its own
 );
 
 ALTER TABLE credential_offer ADD CONSTRAINT credential_offer_pkey PRIMARY KEY (id);
