1529596825dfd1a231723adbaa4a2585c71c2d83
Alexander Ambuehl <alexander.ambuehl@bit.admin.ch>
2024-06-26 16:24:48 +0200

    add context loads test to satisfy sonar

--- src/test/java/ch/admin/bit/eid/issuer_management/Oid4vciApplicationTests.java
+++ src/test/java/ch/admin/bit/eid/issuer_management/Oid4vciApplicationTests.java
@@ -1,15 +1,23 @@
 package ch.admin.bit.eid.issuer_management;
 
+import ch.admin.bit.eid.issuer_management.controllers.CredentialsController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class IssuerManagementApplicationTests {
 
+	@Autowired
+	private CredentialsController credentialsController;
+
 	@Test
 	void contextLoads() {
+		assertThat(credentialsController).isNotNull();
 	}
 
 }
