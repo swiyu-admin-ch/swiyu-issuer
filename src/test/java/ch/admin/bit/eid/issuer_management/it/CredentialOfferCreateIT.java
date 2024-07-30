1d8022da81f6b66ae16d2bea1b393a79786932bf
Bruno Zimmermann <bruno.zimmermann@bit.admin.ch>
2024-07-30 13:15:51 +0200

    Fix test failing due to parsing of Date to Instant

--- src/test/java/ch/admin/bit/eid/issuer_management/it/CredentialOfferCreateIT.java
+++ src/test/java/ch/admin/bit/eid/issuer_management/it/CredentialOfferCreateIT.java
@@ -1,6 +1,5 @@
 package ch.admin.bit.eid.issuer_management.it;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.jsonpath.JsonPath;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.DisplayName;
@@ -9,6 +8,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MvcResult;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static ch.admin.bit.eid.issuer_management.util.DateTimeUtils.ISO8601_FORMAT;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -18,31 +21,29 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @DisplayName("Create Offer")
 class CredentialOfferCreateIT extends BaseIt {
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
-
     @Test
     void testCreateOffer_thenSuccess() throws Exception {
-        String minPayloadWithEmptySubject = String.format("{\"metadata_credential_supported_id\": \"%s\", \"credential_subject_data\": {}}", RandomStringUtils.random(10));
-
+        String minPayloadWithEmptySubject = String.format("{\"metadata_credential_supported_id\": [\"%s\"], \"credential_subject_data\": {\"hello\": \"world\"}}", "test");
         mvc.perform(post(BASE_URL).contentType(MediaType.APPLICATION_JSON).content(minPayloadWithEmptySubject))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.management_id").isNotEmpty())
                 .andExpect(jsonPath("$.offer_deeplink").isNotEmpty());
 
-        String minPayloadWithValidUntil= String.format("{\"metadata_credential_supported_id\": \"%s\", \"credential_subject_data\": {}, \"credential_valid_until\" : \"2010-01-01T19:23:24Z\"}", RandomStringUtils.random(10));
 
+        String now = new SimpleDateFormat(ISO8601_FORMAT).format(new Date());
+        String minPayloadWithValidUntil= String.format("{\"metadata_credential_supported_id\": [\"%s\"], \"credential_subject_data\": {\"hello\": \"world\"}, \"credential_valid_until\" : \"%s\"}", "test", now);
         mvc.perform(post(BASE_URL).contentType(MediaType.APPLICATION_JSON).content(minPayloadWithValidUntil))
                 .andExpect(status().isOk());
     }
 
     @Test
     void testCreateOffer_thenValidationFailure() throws Exception {
-        String emptyMetadataId = String.format("{\"metadata_credential_supported_id\": \"%s\", \"credential_subject_data\": {}}", "");
+        String emptyMetadataId = String.format("{\"metadata_credential_supported_id\": [\"%s\"], \"credential_subject_data\": {}}", "");
 
         mvc.perform(post(BASE_URL).contentType(MediaType.APPLICATION_JSON).content(emptyMetadataId))
                 .andExpect(status().isBadRequest());
 
-        String noCredentialSubject = String.format("{\"metadata_credential_supported_id\": \"%s\", \"credential_subject_data\": null}", "");
+        String noCredentialSubject = String.format("{\"metadata_credential_supported_id\": [\"%s\"], \"credential_subject_data\": null}", "");
 
         mvc.perform(post(BASE_URL).contentType(MediaType.APPLICATION_JSON).content(noCredentialSubject))
                 .andExpect(status().isBadRequest());
@@ -53,12 +54,12 @@ class CredentialOfferCreateIT extends BaseIt {
                 .andExpect(status().isBadRequest());
 
         // Check Invalid JSON payloads in credential_subject_data
-        String invalidValidFromPattern = String.format("{\"metadata_credential_supported_id\": \"%s\", \"credential_subject_data\": {}, \"credential_valid_from\" : \"2010-01-01T19:23:24.111\"}", RandomStringUtils.random(10));
+        String invalidValidFromPattern = String.format("{\"metadata_credential_supported_id\": [\"%s\"], \"credential_subject_data\": {}, \"credential_valid_from\" : \"2010-01-01T19:23:24.111\"}", RandomStringUtils.random(10));
 
         mvc.perform(post(BASE_URL).contentType(MediaType.APPLICATION_JSON).content(invalidValidFromPattern))
                 .andExpect(status().isBadRequest());
 
-        String invalidJson = String.format("{\"metadata_credential_supported_id\": \"%s\", \"credential_subject_data\": {\"test\"}}", RandomStringUtils.random(10));
+        String invalidJson = String.format("{\"metadata_credential_supported_id\": [\"%s\"], \"credential_subject_data\": {\"test\"}}", RandomStringUtils.random(10));
         mvc.perform(post(BASE_URL).contentType(MediaType.APPLICATION_JSON).content(invalidJson))
                 .andExpect(status().isBadRequest());
     }
@@ -70,7 +71,7 @@ class CredentialOfferCreateIT extends BaseIt {
 
         String jsonPayload = """
                         {
-                          "metadata_credential_supported_id": "test",
+                          "metadata_credential_supported_id": ["test"],
                           "credential_subject_data": {
                             "hello": "world"
                           },
