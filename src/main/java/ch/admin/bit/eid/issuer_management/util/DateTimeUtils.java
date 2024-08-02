6dfcc08f581abbbe5a2db6bb6623159bb641e22b
Edgar Zehnder <edgar.zehnder@bit.admin.ch>
2024-08-02 10:42:07 +0200

    [EID-1662] fix iso format

--- src/main/java/ch/admin/bit/eid/issuer_management/util/DateTimeUtils.java
+++ src/main/java/ch/admin/bit/eid/issuer_management/util/DateTimeUtils.java
@@ -5,5 +5,5 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class DateTimeUtils {
 
-    public static final String ISO8601_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+    public static final String ISO8601_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.sss'Z'";
 }
