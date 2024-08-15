0e360a9e0c72276052cbd95a9a4080520f3dc13c
Frithjof Hoppe <frithjof.hoppe@bit.admin.ch>
2024-08-15 16:45:16 +0200

    :wrench: Allow confluence origin to enable PERA to issue inside confluence page

--- src/main/java/ch/admin/bit/eid/issuer_management/config/WebConfig.java
+++ src/main/java/ch/admin/bit/eid/issuer_management/config/WebConfig.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -21,5 +22,12 @@ public class WebConfig implements WebMvcConfigurer {
         public CredentialStatusEnum convert(String source) {
             return CredentialStatusEnum.valueOf(source.toUpperCase());
         }
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://confluence.bit.admin.ch")
+                .allowedMethods("*");
     }
 }
