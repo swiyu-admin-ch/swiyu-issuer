103c4138c5e5bbdc88866df6c5c5670a02e28bef
Zimmermann Bruno BIT <bruno.zimmermann@bit.admin.ch>
2024-12-10 11:03:19 +0100

    fix: FlywayException: Found more than one migration with version 1.0.3

--- src/main/resources/db/migration/h2/afterMigrate.sql
+++ src/main/resources/db/migration/h2/afterMigrate.sql
@@ -1,1 +1,29 @@
 ALTER TABLE credential_offer ALTER COLUMN offer_data text;
+
+DROP TABLE credential_offer_status;
+DROP TABLE status_list;
+CREATE TABLE status_list
+(
+    id              uuid NOT NULL,
+    type            text NOT NULL,
+    config          jsonb,
+    uri             text NOT NULL,
+    status_zipped   text NOT NULL,
+    last_used_index int  NOT NULL,
+    max_length      int  NOT NULL,
+    PRIMARY KEY (id),
+    --
+    -- We want the same constraint name as in postgres!
+    --
+    CONSTRAINT status_list_uri_key unique (uri)
+);
+
+CREATE TABLE credential_offer_status
+(
+    credential_offer_id uuid     NOT NULL,
+    status_list_id      uuid     NOT NULL,
+    index               SMALLINT NOT NULL,
+    PRIMARY KEY (credential_offer_id, status_list_id),
+    FOREIGN KEY (status_list_id) REFERENCES status_list (id),
+    FOREIGN KEY (credential_offer_id) REFERENCES credential_offer (id)
+);
