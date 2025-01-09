f0fa2bcb8f1bfaf13bdad630fb0802dd509c9b72
Edgar Zehnder <edgar.zehnder@bit.admin.ch>
2025-01-09 10:18:29 +0100

    refactor(EID-3004) - remove  unnecessary repo code

--- src/main/java/ch/admin/bj/swiyu/issuer/management/domain/credentialoffer/StatusListRepository.java
+++ src/main/java/ch/admin/bj/swiyu/issuer/management/domain/credentialoffer/StatusListRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
@@ -15,6 +14,4 @@ public interface StatusListRepository extends JpaRepository<StatusList, UUID> {
     List<StatusList> findByUriIn(List<String> uris);
 
     boolean existsByUri(String uri);
-
-    Optional<StatusList> findById(UUID id);
 }
