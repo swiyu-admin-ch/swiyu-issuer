7f752a93838033a36bbc2a5e43bff20628c7ec56
Edgar Zehnder <edgar.zehnder@bit.admin.ch>
2025-02-10 17:18:28 +0100

    feat(EID-3210) : update openapi config

--- src/main/java/ch/admin/bj/swiyu/issuer/management/infrastructure/web/config/OpenApiConfig.java
+++ src/main/java/ch/admin/bj/swiyu/issuer/management/infrastructure/web/config/OpenApiConfig.java
@@ -9,21 +9,18 @@ package ch.admin.bj.swiyu.issuer.management.infrastructure.web.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import lombok.AllArgsConstructor;
 import org.springdoc.core.models.GroupedOpenApi;
-import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @AllArgsConstructor
 @Configuration
 public class OpenApiConfig {
-    private final BuildProperties buildProperties;
 
     @Bean
     public OpenAPI openApi() {
         return new OpenAPI().info(new io.swagger.v3.oas.models.info.Info()
                 .title("Issuer management API")
                 .description("Generic Issuer management service")
-                .version(buildProperties.getVersion())
                 .contact(new io.swagger.v3.oas.models.info.Contact()
                         .name("e-ID - Team Tergum")
                         .email("eid@bit.admin.ch")
