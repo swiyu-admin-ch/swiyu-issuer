dd44b6b91d5dc729315c1809ea2f5e0e06e34970
Gatschet Pascal BIT <pascal.gatschet@bit.admin.ch>
2025-09-09 16:04:06 +0200

    feat(EIDOMNI-256): Refactor interfaces to add @FunctionalInterface annotation and update access modifiers (PMD fixes)

--- issuer-service/src/main/java/ch/admin/bj/swiyu/issuer/domain/credentialoffer/VerifiableCredentialStatusReference.java
+++ issuer-service/src/main/java/ch/admin/bj/swiyu/issuer/domain/credentialoffer/VerifiableCredentialStatusReference.java
@@ -8,6 +8,7 @@ package ch.admin.bj.swiyu.issuer.domain.credentialoffer;
 
 import java.util.Map;
 
+@FunctionalInterface
 public interface VerifiableCredentialStatusReference {
     /**
      * Create a hashmap as to be used in the claims of a verifiable credential
