b3b8d51b43753ecff2358a8d502d0828f73593b8
Edgar Zehnder <edgar.zehnder@bit.admin.ch>
2025-09-16 17:29:35 +0200

    chore(EIDOMNI-225) : add correct json property to field to prevent breaking changes

--- issuer-service/src/main/java/ch/admin/bj/swiyu/issuer/domain/credentialoffer/CredentialOfferMetadata.java
+++ issuer-service/src/main/java/ch/admin/bj/swiyu/issuer/domain/credentialoffer/CredentialOfferMetadata.java
@@ -2,11 +2,13 @@ package ch.admin.bj.swiyu.issuer.domain.credentialoffer;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record CredentialOfferMetadata(
         Boolean deferred,
+        @JsonProperty("vct#integrity") // must be set in order to prevent breaking changes
         String vctIntegrity
 ) {
 }
