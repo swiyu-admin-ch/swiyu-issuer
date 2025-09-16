eebf069a6d359c551bb9099d2a3a202d141b9ed1
Edgar Zehnder <edgar.zehnder@bit.admin.ch>
2025-09-16 13:28:02 +0200

    chore(EIDOMNI-225) : add ignore unknown file

--- issuer-service/src/main/java/ch/admin/bj/swiyu/issuer/domain/credentialoffer/CredentialOfferMetadata.java
+++ issuer-service/src/main/java/ch/admin/bj/swiyu/issuer/domain/credentialoffer/CredentialOfferMetadata.java
@@ -1,5 +1,10 @@
 package ch.admin.bj.swiyu.issuer.domain.credentialoffer;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record CredentialOfferMetadata(
         Boolean deferred,
         String vctIntegrity
