6412c827065430606f3d8af1059653741ce0297e
Edgar Zehnder <edgar.zehnder@bit.admin.ch>
2025-11-26 09:32:02 +0100

    Revert "fix(EIDOMNI-363) : refuse status lists without content length (#140)"
    This reverts commit 90b4f455d874358f44559e2bab78149544307006.

--- issuer-service/src/main/java/ch/admin/bj/swiyu/issuer/service/statusregistry/StatusRegistryContentLengthInterceptor.java
+++ issuer-service/src/main/java/ch/admin/bj/swiyu/issuer/service/statusregistry/StatusRegistryContentLengthInterceptor.java
@@ -6,18 +6,16 @@
 
 package ch.admin.bj.swiyu.issuer.service.statusregistry;
 
+import java.io.IOException;
+
 import ch.admin.bj.swiyu.issuer.common.config.StatusListProperties;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
-
-import java.io.IOException;
-import java.net.URI;
 
 @RequiredArgsConstructor
 @Component
@@ -31,25 +29,10 @@ public class StatusRegistryContentLengthInterceptor implements ClientHttpRequest
         ClientHttpResponse response = execution.execute(request, body);
         long contentLength = response.getHeaders().getContentLength();
 
-        String transferEncoding = response.getHeaders().getFirst(HttpHeaders.TRANSFER_ENCODING);
-
-        // decline if transfer-encoding is chunked
-        if ("chunked".equalsIgnoreCase(transferEncoding)) {
-            throw new IllegalArgumentException(getStatusListSizeUnknownMessage(request.getURI()) + " (chunked transfer encoding)");
-        }
-
-        if (contentLength == -1) {
-            throw new IllegalArgumentException(getStatusListSizeUnknownMessage(request.getURI()));
-        }
-
         if (contentLength > statusListProperties.getStatusListSizeLimit()) {
             throw new IllegalArgumentException("Status list size from %s exceeds maximum allowed size".formatted(request.getURI()));
         }
 
         return response;
-    }
-
-    private String getStatusListSizeUnknownMessage(URI uri) {
-        return "Status list size from %s could not be determined".formatted(uri);
     }
 }
