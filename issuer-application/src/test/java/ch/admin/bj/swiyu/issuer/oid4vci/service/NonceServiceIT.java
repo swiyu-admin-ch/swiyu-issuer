a5ec2ed178356d2698484bfdb6e4b9274efd15c7
VladiStoBIT <vladica.stojic@bit.admin.ch>
2025-12-10 16:07:21 +0100

    EIDOMNI-476: A NonceServiceIT-test added to ensure dropping nonce cache fails in a read-only transactional context

--- issuer-application/src/test/java/ch/admin/bj/swiyu/issuer/oid4vci/service/NonceServiceIT.java
+++ issuer-application/src/test/java/ch/admin/bj/swiyu/issuer/oid4vci/service/NonceServiceIT.java
@@ -4,20 +4,21 @@ import ch.admin.bj.swiyu.issuer.PostgreSQLContainerInitializer;
 import ch.admin.bj.swiyu.issuer.common.config.ApplicationProperties;
 import ch.admin.bj.swiyu.issuer.domain.openid.credentialrequest.holderbinding.SelfContainedNonce;
 import ch.admin.bj.swiyu.issuer.service.NonceService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.orm.jpa.JpaSystemException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Testcontainers
@@ -30,25 +31,43 @@ class NonceServiceIT {
     @Autowired
     private NonceService service;
 
-    @Test
-    void testCachedNonce() {
+    private SelfContainedNonce nonce;
+
+    private SelfContainedNonce expiredNonce;
+
+    @BeforeEach
+    void setUp() {
         var lifetime = applicationProperties.getNonceLifetimeSeconds();
         var nonceDto = service.createNonce();
-        var nonce = new SelfContainedNonce(nonceDto.nonce());
+        nonce = new SelfContainedNonce(nonceDto.nonce());
         assertTrue(nonce.isValid((lifetime)));
         assertFalse(service.isUsedNonce(nonce));
         service.registerNonce(nonce);
         assertTrue(service.isUsedNonce(nonce));
 
-        var expiredNonce = new SelfContainedNonce(UUID.randomUUID() + "::" + Instant.now().minus(lifetime + 5, ChronoUnit.SECONDS));
+        expiredNonce = new SelfContainedNonce(UUID.randomUUID() + "::" + Instant.now().minus(lifetime + 5, ChronoUnit.SECONDS));
         assertFalse(expiredNonce.isValid(lifetime));
         assertFalse(service.isUsedNonce(expiredNonce));
         service.registerNonce(expiredNonce);
         assertTrue(service.isUsedNonce(expiredNonce));
+    }
 
+    @Test
+    void testCachedNonce() {
         // After clearing the cache the expired nonce should be removed, as it is not valid in any check
         service.cleanNonceCache();
+
         assertTrue(service.isUsedNonce(nonce));
         assertFalse(service.isUsedNonce(expiredNonce));
+    }
+
+    @Test
+    @Transactional(readOnly = true)
+    void testCleanNonceCacheWhenTransactionalIsReadOnlyThrowsUncheckedJpaSystemException() {
+
+        // Dropping cache should not really be working at all in a read-only transactional context
+        assertTrue(
+                assertThrowsExactly(JpaSystemException.class, () -> service.cleanNonceCache())
+                        .getMessage().contains("cannot execute DELETE in a read-only transaction"));
     }
 }
