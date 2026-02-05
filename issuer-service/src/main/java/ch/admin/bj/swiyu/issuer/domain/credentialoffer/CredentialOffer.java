d10bb6e5ea030042ceb4c9e00b92371caad90ccc
zib <bruno.zimmermann@bit.admin.ch>
2026-02-05 17:08:33 +0100

    fix(EIDOMNI-665): Fix changing transaction_id. Additional tests for deferred credential flow to ensure response of polling is correct.

--- issuer-service/src/main/java/ch/admin/bj/swiyu/issuer/domain/credentialoffer/CredentialOffer.java
+++ issuer-service/src/main/java/ch/admin/bj/swiyu/issuer/domain/credentialoffer/CredentialOffer.java
@@ -287,4 +287,9 @@ public class CredentialOffer {
         this.credentialStatus = credentialOfferStatus;
     }
 
+    public UUID getOrGenerateTransactionId() {
+        UUID transactionId = Optional.ofNullable(getTransactionId()).orElse(UUID.randomUUID());
+        setTransactionId(transactionId);
+        return transactionId;
+    }
 }
