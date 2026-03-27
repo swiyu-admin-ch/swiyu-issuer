a31cb51498ca5f1bc5ef9138afa382ff38a7e8ea
Edgar Zehnder <edgar.zehnder@bit.admin.ch>
2026-03-27 14:36:54 +0100

    fix(EIDOMNi-824): fix Claims Path Pointer validation for metadata claim descriptor paths and update changelog

---
+++ issuer-service/src/main/java/ch/admin/bj/swiyu/issuer/domain/openid/metadata/PathElementsValidator.java
@@ -0,0 +1,52 @@
+package ch.admin.bj.swiyu.issuer.domain.openid.metadata;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.util.List;
+
+public class PathElementsValidator implements ConstraintValidator<ValidPathElements, List<?>> {
+
+    @Override
+    public boolean isValid(List<?> value, ConstraintValidatorContext context) {
+
+        // must be a non-empty array
+        if (value == null || value.isEmpty()) {
+            return false; // MUST be a non-empty array
+        }
+
+        for (Object o : value) {
+            switch (o) {
+                case null -> {
+                    // is valid, continue
+                }
+                case String ignored -> {
+                    // is valid, continue
+                }
+                case Number n when isValidNumber(n) -> {
+                    // is valid, continue
+                }
+                default -> {
+                    return false; // invalid type
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean isValidNumber(Number n) {
+        double d = n.doubleValue();
+
+        // get the number without any decimals
+        long l = n.longValue();
+
+        // Double allows infinite numbers and NaN which are not valid integers
+        if (!Double.isFinite(d) || Double.isNaN(d)) return false; // not an integer
+
+        // check if the double value is equal to the long value, which means it's an integer
+        if (d != l) return false;
+
+        // check if non-negative integer
+        return l >= 0;
+    }
+}
