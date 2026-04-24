3a18e0f0b6cfcbad91aa7cc9907ddc731e3ecb73
Gatschet Pascal BIT <pascal.gatschet@bit.admin.ch>
2026-04-24 11:47:33 +0200

    chore: update version to 3.0.3 and add JsonIgnoreProperties annotation for improved JSON handling

--- issuer-service/src/main/java/ch/admin/bj/swiyu/issuer/domain/credentialoffer/ClientAgentInfo.java
+++ issuer-service/src/main/java/ch/admin/bj/swiyu/issuer/domain/credentialoffer/ClientAgentInfo.java
@@ -1,5 +1,8 @@
 package ch.admin.bj.swiyu.issuer.domain.credentialoffer;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record ClientAgentInfo(
         String remoteAddr,
         String userAgent,
