76b47b01a973e59c5f25f8464079717fe14dbce6
zib <bruno.zimmermann@bit.admin.ch>
2026-06-02 10:00:22 +0200

    doc(EIDOMNI-648): Provide some information about changed default behaviour in migration guide.

--- issuer-service/src/main/java/ch/admin/bj/swiyu/issuer/domain/openid/metadata/IssuerMetadata.java
+++ issuer-service/src/main/java/ch/admin/bj/swiyu/issuer/domain/openid/metadata/IssuerMetadata.java
@@ -76,11 +76,13 @@ public class IssuerMetadata {
     @JsonProperty("credential_request_encryption")
     @Schema(description = "Object containing information about whether the Credential Issuer supports encryption of the Credential Request on top of TLS.")
     @Valid
+    // Note: This value will be dynamically set during runtime with generated ephemeral keys. It should never be null
     private IssuerCredentialRequestEncryption requestEncryption;
 
     @JsonProperty("credential_response_encryption")
     @Schema(description = "Object containing information about whether the Credential Issuer supports encryption of the Credential Response on top of TLS.")
     @Valid
+    // Note: This value will be dynamically set during runtime with available capabilities. It should never be null.
     private IssuerCredentialResponseEncryption responseEncryption;
 
     @JsonProperty("batch_credential_issuance")
