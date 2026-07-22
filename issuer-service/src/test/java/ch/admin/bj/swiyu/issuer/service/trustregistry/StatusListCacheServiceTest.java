3171dbaf7403edec86ab995f0378f9fab8d0b923
Edgar Zehnder <edgar.zehnder@bit.admin.ch>
2026-07-22 08:02:02 +0200

    fix(EIDOMNI-1182): update generic-java-lib version to 1.8.2 and clean up imports in service classes

--- issuer-service/src/test/java/ch/admin/bj/swiyu/issuer/service/trustregistry/StatusListCacheServiceTest.java
+++ issuer-service/src/test/java/ch/admin/bj/swiyu/issuer/service/trustregistry/StatusListCacheServiceTest.java
@@ -1,30 +1,25 @@
 package ch.admin.bj.swiyu.issuer.service.trustregistry;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import com.nimbusds.jose.JWSAlgorithm;
-import com.nimbusds.jose.jwk.Curve;
-import com.nimbusds.jose.jwk.ECKey;
-import com.nimbusds.jose.jwk.JWKSet;
-import com.nimbusds.jose.jwk.KeyUse;
-import com.nimbusds.jose.jwk.gen.ECKeyGenerator;
-
 import ch.admin.bj.swiyu.issuer.common.config.SwiyuProperties;
 import ch.admin.bj.swiyu.issuer.common.config.SwiyuProperties.TrustRegistryProperties;
 import ch.admin.bj.swiyu.issuer.service.did.DidKeyResolverFacade;
 import ch.admin.bj.swiyu.issuer.service.statusregistry.StatusRegistryClient;
 import ch.admin.bj.swiyu.issuer.service.trustregistry.fixtures.StatusListGenerator;
 import ch.admin.bj.swiyu.jwtvalidator.DidJwtValidator;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.jwk.Curve;
+import com.nimbusds.jose.jwk.ECKey;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.KeyUse;
+import com.nimbusds.jose.jwk.gen.ECKeyGenerator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
 
 public class StatusListCacheServiceTest {
 
@@ -41,8 +36,8 @@ public class StatusListCacheServiceTest {
         swiyuProperties = mock(SwiyuProperties.class);
         trustRegistryProperties = mock(TrustRegistryProperties.class);
         when(swiyuProperties.trustRegistry()).thenReturn(trustRegistryProperties);
-        when(trustRegistryProperties.maxCacheSize()).thenReturn(1000l);
-        when(trustRegistryProperties.requestBackoffSeconds()).thenReturn(100l);
+        when(trustRegistryProperties.maxCacheSize()).thenReturn(1000L);
+        when(trustRegistryProperties.requestBackoffSeconds()).thenReturn(100L);
         didJwtValidator = mock(DidJwtValidator.class);
         issuerPublicKeyLoader = mock(DidKeyResolverFacade.class);
         statusListResolver = mock(StatusRegistryClient.class);
@@ -53,13 +48,13 @@ public class StatusListCacheServiceTest {
      */
     @Test
     void testGetTokenStatusListTokenByUri() throws Exception {
-        when(trustRegistryProperties.maxCacheTtlSeconds()).thenReturn(500l);
+        when(trustRegistryProperties.maxCacheTtlSeconds()).thenReturn(500L);
         cacheService = new StatusListCacheService(swiyuProperties, didJwtValidator, issuerPublicKeyLoader, statusListResolver);
         ECKey testKey = new ECKeyGenerator(Curve.P_256)
-            .algorithm(JWSAlgorithm.ES256)
-            .keyID("did:webvh:example.com#key-1")
-            .keyUse(KeyUse.SIGNATURE)
-            .generate();
+                .algorithm(JWSAlgorithm.ES256)
+                .keyID("did:webvh:example.com#key-1")
+                .keyUse(KeyUse.SIGNATURE)
+                .generate();
         when(issuerPublicKeyLoader.resolveKey(eq(testKey.getKeyID()))).thenReturn(testKey.toPublicJWK());
         var statusListJwt = StatusListGenerator.createTokenStatusListTokenVerifiableCredential(StatusListGenerator.SPEC_STATUS_LIST, testKey, "did:example", testKey.getKeyID());
         when(statusListResolver.resolveStatusList(eq(StatusListGenerator.SPEC_SUBJECT))).thenReturn(statusListJwt);
@@ -67,7 +62,7 @@ public class StatusListCacheServiceTest {
         var statusList = assertDoesNotThrow(() -> cacheService.getTokenStatusListTokenByUri(StatusListGenerator.SPEC_SUBJECT));
         assertDoesNotThrow(() -> cacheService.getTokenStatusListTokenByUri(StatusListGenerator.SPEC_SUBJECT));
 
-        verify(didJwtValidator, times(1)).validateJwt(eq(statusListJwt), any(JWKSet.class));
+        verify(didJwtValidator, times(1)).validateJwt(eq(statusListJwt), any(JWK.class));
         assertThat(statusList).isNotNull();
         assertThat(statusList.getStatusList()).isNotNull();
         assertThat(statusList.getExp()).isNotNull().isNotZero();
@@ -80,14 +75,14 @@ public class StatusListCacheServiceTest {
      */
     @Test
     void testGetTokenStatusListTokenByUri_noCache() throws Exception {
-        when(trustRegistryProperties.maxCacheTtlSeconds()).thenReturn(0l);
+        when(trustRegistryProperties.maxCacheTtlSeconds()).thenReturn(0L);
         // Must create cache serivce here, as when initiated the TTL is set for the cache
         cacheService = new StatusListCacheService(swiyuProperties, didJwtValidator, issuerPublicKeyLoader, statusListResolver);
         ECKey testKey = new ECKeyGenerator(Curve.P_256)
-            .algorithm(JWSAlgorithm.ES256)
-            .keyID("did:webvh:example.com#key-1")
-            .keyUse(KeyUse.SIGNATURE)
-            .generate();
+                .algorithm(JWSAlgorithm.ES256)
+                .keyID("did:webvh:example.com#key-1")
+                .keyUse(KeyUse.SIGNATURE)
+                .generate();
         when(issuerPublicKeyLoader.resolveKey(eq(testKey.getKeyID()))).thenReturn(testKey.toPublicJWK());
         var statusListJwt = StatusListGenerator.createTokenStatusListTokenVerifiableCredential(StatusListGenerator.SPEC_STATUS_LIST, testKey, "did:example", testKey.getKeyID());
 
@@ -102,6 +97,6 @@ public class StatusListCacheServiceTest {
         // Second Invocation to prove nothing is cached
         // Note: cache.getEstimatedSize() is flaky
         assertDoesNotThrow(() -> cacheService.getTokenStatusListTokenByUri(StatusListGenerator.SPEC_SUBJECT));
-        verify(didJwtValidator, times(2)).validateJwt(eq(statusListJwt), any(JWKSet.class));
+        verify(didJwtValidator, times(2)).validateJwt(eq(statusListJwt), any(JWK.class));
     }
 }
