483822ebbd85ee8cc873d3c72b2b018d64d4597e
Edgar Zehnder <edgar.zehnder@bit.admin.ch>
2026-08-12 13:56:03 +0200

    fix(EIDOMNI-1230): add JsonIgnoreProperties annotation to CredentialResponseEncryptionClass for better JSON handling

--- issuer-service/src/main/java/ch/admin/bj/swiyu/issuer/domain/openid/credentialrequest/CredentialResponseEncryptionClass.java
+++ issuer-service/src/main/java/ch/admin/bj/swiyu/issuer/domain/openid/credentialrequest/CredentialResponseEncryptionClass.java
@@ -4,6 +4,7 @@ import ch.admin.bj.swiyu.issuer.common.exception.CredentialRequestError;
 import ch.admin.bj.swiyu.issuer.common.exception.Oid4vcException;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.nimbusds.jose.jwk.JWK;
 import lombok.Data;
 
@@ -14,27 +15,29 @@ import java.util.Map;
 
 @Validated
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class CredentialResponseEncryptionClass {
-    private  Map<String, Object> jwk;
-    private  String enc;
+    private Map<String, Object> jwk;
+    private String enc;
 
     public CredentialResponseEncryptionClass(Map<String, Object> jwkJson, String enc) {
         this.jwk = jwkJson;
         this.enc = enc;
     }
-    
+
     /**
      * Gets the encryption algorithm from the enclosed Json Web Key
+     *
      * @return the encryption key algorithm as string
      */
     @JsonIgnore
     public String extractAlg() {
         try {
-        var parsedJwk = JWK.parse(this.jwk);
-        if (parsedJwk.getAlgorithm() == null) {
-            throw new Oid4vcException(CredentialRequestError.INVALID_ENCRYPTION_PARAMETERS, "Encryption JWK must have a JWE algorithm specified");
-        }
-        return parsedJwk.getAlgorithm().getName();
+            var parsedJwk = JWK.parse(this.jwk);
+            if (parsedJwk.getAlgorithm() == null) {
+                throw new Oid4vcException(CredentialRequestError.INVALID_ENCRYPTION_PARAMETERS, "Encryption JWK must have a JWE algorithm specified");
+            }
+            return parsedJwk.getAlgorithm().getName();
         } catch (ParseException e) {
             throw new Oid4vcException(e, CredentialRequestError.INVALID_ENCRYPTION_PARAMETERS, "Encryption JWK cannot be parsed.");
         }
