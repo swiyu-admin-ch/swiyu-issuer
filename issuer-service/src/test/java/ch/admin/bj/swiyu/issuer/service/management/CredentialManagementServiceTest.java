d3fefb228d451efde17dda80d04c8308e54b554c
edgar-bit <142200976+edgar-bit@users.noreply.github.com>
2026-08-21 13:39:40 +0200

    feat(EIDOMNI-680): add RenewalRequest and RenewalResponse DTOs to Ope… (#493)
    * feat(EIDOMNI-680): add RenewalRequest and RenewalResponse DTOs to OpenAPI spec and update OpenApiConfig
    
    * feat(EIDOMNI-680): refactor RenewalRequestDto and RenewalResponseDto package structure and update imports

--- issuer-service/src/test/java/ch/admin/bj/swiyu/issuer/service/management/CredentialManagementServiceTest.java
+++ issuer-service/src/test/java/ch/admin/bj/swiyu/issuer/service/management/CredentialManagementServiceTest.java
@@ -15,10 +15,10 @@ import ch.admin.bj.swiyu.issuer.dto.credentialofferstatus.CredentialStatusTypeDt
 import ch.admin.bj.swiyu.issuer.dto.credentialofferstatus.StatusResponseDto;
 import ch.admin.bj.swiyu.issuer.dto.credentialofferstatus.UpdateCredentialStatusRequestTypeDto;
 import ch.admin.bj.swiyu.issuer.dto.credentialofferstatus.UpdateStatusResponseDto;
+import ch.admin.bj.swiyu.issuer.dto.renewal.RenewalResponseDto;
 import ch.admin.bj.swiyu.issuer.service.CredentialStateService;
 import ch.admin.bj.swiyu.issuer.service.offer.CredentialOfferValidationService;
 import ch.admin.bj.swiyu.issuer.service.persistence.CredentialPersistenceService;
-import ch.admin.bj.swiyu.issuer.service.renewal.RenewalResponseDto;
 import ch.admin.bj.swiyu.issuer.service.statuslist.StatusListOrchestrator;
 import com.google.gson.JsonParser;
 import org.jetbrains.annotations.NotNull;
